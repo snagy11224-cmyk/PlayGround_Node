e63d279747126c8f878fc814711ddb55d937f4b8
snagy11224-cmyk <snagy11224@gmail.com>
2026-01-11 18:19:05 +0200

    added register and login endpoints using JWT

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require("express"); //importing express module
 const app = express(); //app is an object returned by express function
+const bcrypt = require("bcrypt"); 
+const jwt = require("jsonwebtoken");
 app.use(express.json()); //to make the app able to parse json data
 const port = 4000; //open port --> to communicate with ip address (host)
+
+
+//in-memory data
+let users = [   
+    { email: "salma@salma.salma", password: "1234" }
+]
+
+//secret keysn for JWT
+const ACCESS_SECRET="mysecretkey";
+const REFRESH_SECRET="myrefreshsecretkey";
+
+
+/*################################################################################*/
 
 //get endpoint
 //req,res are built in express objects
@@ -15,16 +30,45 @@ const name=req.body.name;  //getting name from request body
 res.status(201).json({ message: `Hello ${name}` });
 });
 
-//patch endpoint
-app.patch("/", (req, res) => {
-  res.send("Hello from Patch request");
+/*################################################################################*/
+
+//register endpoint
+app.post("/users", async (req, res) => {
+  const { email,password } = req.body;    
+  const hashedPassword = await bcrypt.hash(password, 10); //10 is the salt => # of rounds to hash the password
+  const user={email,password:hashedPassword};
+  console.log(user.password);
+  users.push({user});  
+  res.status(201).json({ message: "User Created Successfully!", user: user });
 });
 
-//delete endpoint
-app.delete("/", (req, res) => {
-  res.send("Hello from Delete request");
-}); 
+//login endpoint
+app.post("/users/login", async (req, res) => {
+  const { email, password } = req.body;
+   //check ifn user exists in DB
+   const user = users.find((u) => u.email === email);
+    if (!user) {
+        return res.status(401).json({ message: "Invalid credentials" });
+    }
+    //check if password is correct
+    const isCorrectPassword = await bcrypt.compare(password, user.password);
+    if (!isCorrectPassword) {
+        return res.status(401).json({ message: "Invalid credentials" });
+    }
+    //generate JWT tokens
+    const accessToken = createAceessToken(user);
+    const refreshToken = createRefreshToken(user);
+});
 
+/*################################################################################*/
+function createAceessToken(user){
+    return jwt.sign({email:user.email},ACCESS_SECRET,{expiresIn:"15m"});
+}
+function createRefreshToken(user){
+    return jwt.sign({email:user.email},REFRESH_SECRET,{expiresIn:"7d"});
+}
+
+/*################################################################################*/
 //to make the server listen on the specified port
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
